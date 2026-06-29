8a80564947e4e2e626fc8ac2d1fa485c19285143
dxmario0 <dongxuedarden@gmail.com>
2026-06-29 14:21:56 -0700

    Add settings module

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+"""
+Global configuration for GEO MVP.
+
+This file will contain project paths,
+API initialization,
+model configuration,
+and shared constants migrated
+from the Colab notebook.
+"""
